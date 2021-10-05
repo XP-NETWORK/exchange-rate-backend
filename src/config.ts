05c869345b8921789a1a7d5d81988f8a4b41e4fe
rupansh-arch <rupanshsekar@hotmail.com>
2021-10-05 21:59:34 +0530

    lintcheck

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,16 @@
 require('dotenv').config();
 
 if (!process.env.CMC_API_KEY) {
-    throw Error("Coin Market Cap Api Key required!");
+    throw Error('Coin Market Cap Api Key required!');
 }
 
-console.warn("WARN: using permissive cors!!")
+console.warn('WARN: using permissive cors!!');
 
 export default {
     port: 3030,
-    cors: "*",
-    cmc_api: "https://pro-api.coinmarketcap.com/",
+    cors: '*',
+    cmc_api: 'https://pro-api.coinmarketcap.com/',
     cmc_key: process.env.CMC_API_KEY,
     rate_limit_ms: 10 * 60 * 1000, // 10 mins
-    rate_limit_reqs: 1000
+    rate_limit_reqs: 1000,
 };
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,36 @@
-import express, { Response } from "express";
-import cors from "cors";
-import config from "./config";
-import { cmcCachedExchange } from "./singletons/cmc";
-import { SupportedCurrency } from "crypto-exchange-rate/dist/model/domain/SupportedCurrency";
-import rateLimit from "express-rate-limit";
+import express, { Response } from 'express';
+import cors from 'cors';
+import config from './config';
+import { cmcCachedExchange } from './singletons/cmc';
+import { SupportedCurrency } from 'crypto-exchange-rate/dist/model/domain/SupportedCurrency';
+import rateLimit from 'express-rate-limit';
 
 const app = express();
 
 app.use(cors({ origin: config.cors }));
 app.use(express.json());
-app.use(rateLimit({
-    windowMs: config.rate_limit_ms,
-    max: config.rate_limit_reqs
-}))
+app.use(
+    rateLimit({
+        windowMs: config.rate_limit_ms,
+        max: config.rate_limit_reqs,
+    })
+);
 
 type BatchDataResp = {
-    [key in SupportedCurrency]: number
-}
+    [key in SupportedCurrency]: number;
+};
 
-app.get("/batch_data", async (_, res: Response<BatchDataResp>) => {
-    const cache = await cmcCachedExchange.getBatchedRate(Object.values(SupportedCurrency));
+app.get('/batch_data', async (_, res: Response<BatchDataResp>) => {
+    const cache = await cmcCachedExchange.getBatchedRate(
+        Object.values(SupportedCurrency)
+    );
     const entries = Object.fromEntries(cache) as BatchDataResp;
 
-    res.set('Cache-Control', `public, max-age=${cmcCachedExchange.getCacheExpiry()/1000}`)
+    res.set(
+        'Cache-Control',
+        `public, max-age=${cmcCachedExchange.getCacheExpiry() / 1000}`
+    );
     res.send(entries);
 });
 
-app.listen(config.port, () => `server listening @${config.port}`)
+app.listen(config.port, () => `server listening @${config.port}`);
