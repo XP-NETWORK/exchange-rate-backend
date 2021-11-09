fb62638db0f3c64aa7623f00d76d013b87fd73ae
rupansh <rupanshsekar@hotmail.com>
2021-11-09 14:37:28 +0530

    singletons: cmc: use root import for everything

--- src/singletons/cmc.ts
+++ src/singletons/cmc.ts
@@ -3,7 +3,6 @@ import {
     cachedExchangeRateRepo,
 } from 'crypto-exchange-rate';
 import { CMCModel } from 'crypto-exchange-rate';
-import { exchangeRateDtoMapper } from 'crypto-exchange-rate/dist/model/coinmarketcap/ExchangeRateDtoMapper';
 import config from '../config';
 
 const idMapper = CMCModel.idMapper();
@@ -12,6 +11,6 @@ export const cmcCachedExchange = cachedExchangeRateRepo(
     cmcBatchExchangeRepo(
         CMCModel.batchExchangeRateService(config.cmc_api, config.cmc_key),
         idMapper,
-        exchangeRateDtoMapper(idMapper)
+        CMCModel.exchangeRateDtoMapper(idMapper)
     )
 );
